08f4481706492360e4e4fd4e09f5e5a206c7a9df
SakovichIgor <nonord1995@gmail.com>
2023-05-08 21:23:27 +0300

    catalog

--- maket_loft/js/catalog_item.js
+++ maket_loft/js/catalog_item.js
@@ -1,11 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 5,
+    spaceBetween: 10,
     slidesPerView: 5,
     freeMode: true,
     watchSlidesProgress: true,
   });
   var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 5,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
