e8c1f55dda7f7a7cb3a136d7f5da5b48a646007e
SakovichIgor <nonord1995@gmail.com>
2023-05-24 19:17:40 +0300

    chan

--- maket_loft/js/catalog_item.js
+++ maket_loft/js/catalog_item.js
@@ -1,13 +1,14 @@
-$("#slider-range").slider({
-  range: true,
-  min: 1000,
-  max: 200000,
-  values: [50000, 102000],
-  slide: function (event, ui) {
-    $("#amount1").val(ui.values[0] + " ₽");
-    $("#amount2").val(ui.values[1] + " ₽");
-  }
-});
+$("#slider-range")
+  .slider({
+    range: true,
+    min: 1000,
+    max: 200000,
+    values: [50000, 102000],
+    slide: function (event, ui) {
+      $("#amount1").val(ui.values[0] + " ₽");
+      $("#amount2").val(ui.values[1] + " ₽");
+    }
+  });
 
 $("#amount1").val($("#slider-range ").slider("values", 0) + " ₽");
 $("#amount2").val($("#slider-range ").slider("values", 1) + " ₽");
