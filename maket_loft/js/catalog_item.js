7eeb10df9dc2a48df8d2f4d34165d94b1a79d3b6
SakovichIgor <nonord1995@gmail.com>
2023-05-24 16:41:11 +0300

    add filter

--- maket_loft/js/catalog_item.js
+++ maket_loft/js/catalog_item.js
@@ -3,30 +3,36 @@ $("#slider-range").slider({
   min: 1000,
   max: 200000,
   values: [50000, 102000],
-  slide: function( event, ui ) {
-    $( "#amount1" ).val(ui.values[ 0 ] + " ₽");
-    $("#amount2").val(ui.values[ 1 ] + " ₽");
+  slide: function (event, ui) {
+    $("#amount1").val(ui.values[0] + " ₽");
+    $("#amount2").val(ui.values[1] + " ₽");
   }
 });
 
-$( "#amount1" ).val($( "#slider-range ").slider( "values", 0 ) + " ₽" );
-$( "#amount2" ).val($( "#slider-range ").slider( "values", 1 )  + " ₽" );
+$("#amount1").val($("#slider-range ").slider("values", 0) + " ₽");
+$("#amount2").val($("#slider-range ").slider("values", 1) + " ₽");
 
 
 
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+const sortBtn = document.querySelector(".sort__btn");
+const sortMenu = document.querySelector(".sort__menu")
+sortBtn.addEventListener('click', function () {
+  sortMenu.classList.toggle('sort__menu_active')
+});
